16dfd816c5946e57a571fa13ab722a90f88461f0
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2024-07-30 23:36:15 +0200

    Add playform compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,24 @@
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import liciousI18n from '@astrolicious/i18n';
+import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
-import icon from 'astro-icon';
+import playformCompress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
-    tailwind({ applyBaseStyles: false }),
+
     icon(),
+
+    playformCompress(),
+
+    tailwind({
+      applyBaseStyles: false,
+    }),
+
     liciousI18n({
       locales: ['en', 'mk'],
       defaultLocale: 'en',
