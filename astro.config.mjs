60caca01289329fd0d243cde5591cebb805a9ce5
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2024-07-30 23:43:13 +0200

    Add favicon, sitemap setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,23 @@
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import liciousI18n from '@astrolicious/i18n';
+import playformCompress from '@playform/compress';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
-import playformCompress from '@playform/compress';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://feit-code.com',
   integrations: [
     react(),
-
     icon(),
-
     playformCompress(),
-
+    sitemap(),
     tailwind({
       applyBaseStyles: false,
     }),
-
     liciousI18n({
       locales: ['en', 'mk'],
       defaultLocale: 'en',
