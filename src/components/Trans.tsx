de993d465ae3d59946e9f1ee2a20674e23fb912a
Daniel Shijakovski <dshijakovski@yahoo.com>
2024-08-03 01:29:34 +0200

    Setup Trans component, new yarn version

---
+++ src/components/Trans.tsx
@@ -0,0 +1,13 @@
+import type { ComponentProps } from 'react'
+import { Trans as ReactTrans } from 'react-i18next'
+
+import { t } from 'i18n:astro'
+import i18next from 'i18next'
+
+type Props = { tKey: Parameters<typeof t>[0] } & Pick<ComponentProps<typeof ReactTrans>, 'values' | 'components'>
+
+const Trans = ({ tKey, ...rest }: Props) => {
+  return <ReactTrans i18n={i18next} defaults={t(tKey[0] as any)} {...rest} />
+}
+
+export default Trans
