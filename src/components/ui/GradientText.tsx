ff338795c72aac5e79eb0f224edaeca03f32bb0f
Daniel Shijakovski <dshijakovski@yahoo.com>
2024-08-03 02:12:29 +0200

    Add hero section

---
+++ src/components/ui/GradientText.tsx
@@ -0,0 +1,29 @@
+import type { PropsWithChildren } from 'react'
+
+import clsx from 'clsx'
+
+import type { As } from '@nextui-org/react'
+
+type Props = {
+  as?: As
+  className?: string
+  variant?: 'primary' | 'secondary'
+} & PropsWithChildren
+
+const GradientText = ({ as: componentType = 'span', className = '', variant = 'primary', children }: Props) => {
+  const Element = componentType
+
+  return (
+    <Element
+      className={clsx(
+        'bg-clip-text text-transparent',
+        variant === 'primary' ? 'gradient-primary' : 'gradient-secondary',
+        className,
+      )}
+    >
+      {children}
+    </Element>
+  )
+}
+
+export default GradientText
