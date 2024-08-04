3de4a3adc05a74562750ef7a1bea697b1382a0ed
Daniel Shijakovski <dshijakovski@yahoo.com>
2024-08-04 03:52:33 +0200

    Fix spacing on main page

--- src/components/landing-page/Testimonials/index.tsx
+++ src/components/landing-page/Testimonials/index.tsx
@@ -6,7 +6,7 @@ import GradientText from '@components/ui/GradientText'
 
 const Testimonials = ({ children }: PropsWithChildren) => {
   return (
-    <div className="flex items-start justify-between gap-8 lg:flex-col lg:items-center lg:text-center lg:gap-16">
+    <div className="flex items-start justify-between gap-8 lg:flex-col lg:items-center lg:text-center lg:gap-12">
       <div className="space-y-1">
         <h2 className="text-5xl font-medium sm:text-4xl">{t('landing:TESTIMONIALS.HEADING')}</h2>
 
