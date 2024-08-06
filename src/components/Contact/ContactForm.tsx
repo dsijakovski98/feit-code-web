a461f1b35b671d7812145f0b8e8015473cdfd542
Daniel Shijakovski <dshijakovski@yahoo.com>
2024-08-06 17:33:54 +0200

    Finish contact form

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -1,4 +1,5 @@
 import { t } from 'i18n:astro'
+import { useEffect, useRef, useState } from 'react'
 import { Controller, type SubmitHandler, useForm } from 'react-hook-form'
 import type { Slots } from 'types/index'
 import { type InferInput, email, nonEmpty, object, pipe, string, trim } from 'valibot'
@@ -25,11 +26,18 @@ const ContactForm = ({ ...rest }: Props) => {
 
   const slots = rest as Slots<'error' | 'success'>
 
+  const [formHeight, setFormHeight] = useState(0)
+  const formRef = useRef<HTMLFormElement | null>(null)
+
+  useEffect(() => {
+    if (!formRef.current) return
+    setFormHeight(formRef.current.offsetHeight)
+  }, [])
+
   const {
     handleSubmit,
     control,
     setError,
-    setValue,
     clearErrors,
     formState: { errors, isSubmitting, isSubmitSuccessful },
   } = useForm({
@@ -53,9 +61,7 @@ const ContactForm = ({ ...rest }: Props) => {
         },
       )
       .then(
-        (result) => {
-          console.log(result)
-        }, // Can use as success scenario handling if needed
+        () => {}, // Can use as success scenario handling if needed
         (error) => {
           console.log({ error })
           setError('root', { message: 'CONTACT' })
@@ -65,9 +71,12 @@ const ContactForm = ({ ...rest }: Props) => {
 
   if (isSubmitSuccessful) {
     return (
-      <div className="relative h-full grid place-items-center text-center content-center">
-        <div className="absolute top-20 right-10">{slots.success}</div>
-        <h2 role="alert" className="text-3xl text-balance">
+      <div
+        className="relative h-full min-h-[500px] grid place-items-center text-center content-center"
+        style={{ minHeight: `${formHeight}px` }}
+      >
+        <div className="absolute top-20 sm:top-10 right-10">{slots.success}</div>
+        <h2 role="alert" className="text-3xl text-balance sm:text-wrap">
           {t('contact:REACH_OUT.SUCCESS')}
         </h2>
       </div>
@@ -75,8 +84,8 @@ const ContactForm = ({ ...rest }: Props) => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="h-full flex flex-col justify-between gap-14">
-      <div className="flex flex-col gap-8 flex-1">
+    <form ref={formRef} onSubmit={handleSubmit(onSubmit)} className="h-full flex flex-col justify-between gap-14">
+      <div className="flex flex-col gap-8 flex-1 sm:mb-2">
         <Controller
           name="name"
           control={control}
@@ -85,8 +94,6 @@ const ContactForm = ({ ...rest }: Props) => {
             <div className="relative">
               <Input
                 {...field}
-                isClearable
-                onClear={() => setValue('name', '')}
                 color="primary"
                 variant="bordered"
                 autoComplete="name"
@@ -94,8 +101,8 @@ const ContactForm = ({ ...rest }: Props) => {
                 placeholder={tForm.NAME.PLACEHOLDER}
                 isInvalid={!!errors.name?.message}
                 classNames={{
-                  inputWrapper: 'rounded-xl h-[2.5lh] shadow-lg border-slate-600',
-                  label: 'text-medium font-semibold !text-slate-50',
+                  inputWrapper: 'rounded-xl h-[2.7lh] shadow-lg border-slate-600',
+                  label: 'text-medium font-semibold sm:font-normal !text-slate-50',
                   input:
                     'font-inter text-medium text-slate-50 placeholder:text-medium placeholder:font-kanit placeholder:text-slate-400 placeholder:font-extralight',
                 }}
@@ -103,7 +110,7 @@ const ContactForm = ({ ...rest }: Props) => {
               {errors.name?.message && (
                 <p
                   role="alert"
-                  className="absolute inset-x-2 top-full flex translate-y-1 items-center gap-1 leading-[1.2] text-danger-500 sm:inset-x-8 sm:translate-y-0 sm:items-start"
+                  className="absolute inset-x-2 top-full flex translate-y-1 items-center gap-1 leading-[1.2] text-danger-500 sm:text-sm sm:[&_svg]:!scale-[0.7] sm:gap-0"
                 >
                   {slots.error} {t(`common:ERRORS.${errors.name.message}` as any)}
                 </p>
@@ -120,8 +127,6 @@ const ContactForm = ({ ...rest }: Props) => {
             <div className="relative">
               <Input
                 {...field}
-                isClearable
-                onClear={() => setValue('email', '')}
                 color="primary"
                 variant="bordered"
                 autoComplete="email"
@@ -129,8 +134,8 @@ const ContactForm = ({ ...rest }: Props) => {
                 placeholder={tForm.EMAIL.PLACEHOLDER}
                 isInvalid={!!errors.email?.message}
                 classNames={{
-                  inputWrapper: 'rounded-xl h-[2.5lh] shadow-lg border-slate-600',
-                  label: 'text-medium font-semibold !text-slate-50',
+                  inputWrapper: 'rounded-xl h-[2.7lh] shadow-lg border-slate-600',
+                  label: 'text-medium font-semibold sm:font-normal !text-slate-50',
                   input:
                     'font-inter text-medium !text-slate-50 placeholder:text-medium placeholder:font-kanit placeholder:text-slate-400 placeholder:font-extralight',
                 }}
@@ -138,7 +143,7 @@ const ContactForm = ({ ...rest }: Props) => {
               {errors.email?.message && (
                 <p
                   role="alert"
-                  className="absolute inset-x-2 top-full flex translate-y-1 items-center gap-1 leading-[1.2] text-danger-500 sm:inset-x-8 sm:translate-y-0 sm:items-start"
+                  className="absolute inset-x-2 top-full flex translate-y-1 items-center gap-1 leading-[1.2] text-danger-500 sm:text-sm sm:[&_svg]:!scale-[0.7] sm:gap-0"
                 >
                   {slots.error} {t(`common:ERRORS.${errors.email.message}` as any)}
                 </p>
@@ -164,15 +169,15 @@ const ContactForm = ({ ...rest }: Props) => {
                 isInvalid={!!errors.message?.message}
                 classNames={{
                   inputWrapper: 'rounded-xl border-2 shadow-lg border-slate-600',
-                  label: 'text-lg font-semibold !text-slate-50',
+                  label: 'text-lg font-semibold sm:font-normal !text-slate-50',
                   input:
-                    'font-inter text-medium leading-[1.3] placeholder:text-medium placeholder:font-kanit placeholder:font-extralight',
+                    'font-inter text-medium leading-[1.3] placeholder:text-medium placeholder:font-kanit placeholder:text-slate-400 placeholder:font-extralight',
                 }}
               />
               {errors.message?.message && (
                 <p
                   role="alert"
-                  className="absolute inset-x-2 top-full flex translate-y-1 items-center gap-1 leading-[1.2] text-danger-500 sm:inset-x-8 sm:translate-y-0 sm:items-start"
+                  className="absolute inset-x-2 top-full flex translate-y-1 items-center gap-1 leading-[1.2] text-danger-500 sm:text-sm sm:[&_svg]:!scale-[0.7] sm:gap-0"
                 >
                   {slots.error} {t(`common:ERRORS.${errors.message.message}` as any)}
                 </p>
@@ -186,7 +191,7 @@ const ContactForm = ({ ...rest }: Props) => {
         {errors.root?.message && (
           <p
             role="alert"
-            className="leading-[1.2] text-center flex items-center justify-center gap-1 text-danger-500 text-lg sm:inset-x-8 sm:translate-y-0 sm:items-start"
+            className="leading-[1.2] sm:text-start text-center flex items-center justify-center gap-1 text-danger-500 text-lg sm:inset-x-8 sm:translate-y-0 sm:items-start sm:gap-2 sm:[&_svg]:scale-[1.2]"
           >
             {slots.error} {t(`common:ERRORS.${errors.root.message}` as any)}
           </p>
