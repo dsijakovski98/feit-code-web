1220ad4027e96d41424083f8c4434afd2ade0ce6
Daniel Shijakovski <dshijakovski@yahoo.com>
2024-08-06 03:47:19 +0200

    Setup contact page & form

---
+++ src/components/Contact/ContactForm.tsx
@@ -0,0 +1,17 @@
+const ContactForm = () => {
+  // TODO: Implement inputs
+  // TODO: Implement error handling
+  // TODO: Implement submission
+  return (
+    <form noValidate className="h-full debug-full flex flex-col justify-between">
+      <div>
+        <p>Name here</p>
+        <p>Email here</p>
+        <p>Message here</p>
+      </div>
+      <p className="mt-auto">Submit button here</p>
+    </form>
+  )
+}
+
+export default ContactForm
